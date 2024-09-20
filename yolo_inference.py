d207fbd3d6472984542cc4a5ae91b92d4ebd5efe
Sameer pareek <sameerpareek500@gmail.com>
2024-09-20 17:13:10 +0530

    made models and added them using collab

--- yolo_inference.py
+++ yolo_inference.py
@@ -6,4 +6,4 @@ results = model.predict('Input_videos/08fd33_4.mp4',save = True)
 print(results[0])
 print('=============================')
 for box in results[0].boxes:
-    print(box)
+    print(box) 
